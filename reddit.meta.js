10924c6fb6302c3340035f088f82fa5052371daa
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-14 19:29:20 +0000

    Created reddit.meta.js

---
+++ reddit.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Kanboard
+// @namespace   http://www.benjaminsproule.com
+// @author      Benjamin Sproule
+// @include     http://*.reddit.com/*
+// @include     https://*.reddit.com/*
+// @match       http://*.reddit.com/*
+// @match       https://*.reddit.com/*
+// @downloadURL https://github.com/gigaSproule/user-scripts/raw/master/reddit.user.js
+// @updateURL   https://github.com/gigaSproule/user-scripts/raw/master/reddit.meta.js
+// @version     1.0.1
+// @run-at      document-end
+// ==/UserScript==
