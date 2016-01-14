64b330b9883d57515e8c48f760b809f632588057
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-14 17:32:53 +0000

    Updated metadata block

--- kanboard.meta.js
+++ kanboard.meta.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name        Kanboard
-// @namespace   com.benjaminsproule
+// @namespace   http://www.benjaminsproule.com
+// @author      Benjamin Sproule
 // @include     http://*/kanboard*
+// @match       http://*/kanboard*
 // @downloadURL https://github.com/gigaSproule/user-scripts/raw/master/kanboard.user.js
 // @updateURL   https://github.com/gigaSproule/user-scripts/raw/master/kanboard.meta.js
-// @version     1
-// @grant       none
+// @version     1.0.0
+// @run-at      document-end
 // ==/UserScript==
