672ec4a8588f78b97ca1cdf4b44504015c9215c7
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-14 19:26:37 +0000

    Update blogspot.meta.js

--- blogspot.meta.js
+++ blogspot.meta.js
@@ -3,7 +3,7 @@
 // @namespace   http://www.benjaminsproule.com
 // @author      Benjamin Sproule
 // @include     http://*.blogspot.co.uk/*
-// @match     http://*.blogspot.co.uk/*
+// @match       http://*.blogspot.co.uk/*
 // @downloadURL https://github.com/gigaSproule/user-scripts/raw/master/blogspot.user.js
 // @updateURL   https://github.com/gigaSproule/user-scripts/raw/master/blogspot.meta.js
 // @version     1.0.0
