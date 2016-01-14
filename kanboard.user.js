070b59949d2dcd8a3327a4dc021ee6f6d42524d3
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-14 17:10:58 +0000

    Added @updateURL

--- kanboard.user.js
+++ kanboard.user.js
@@ -3,6 +3,7 @@
 // @namespace   com.benjaminsproule
 // @include     http://*/kanboard*
 // @downloadURL https://github.com/gigaSproule/user-scripts/raw/master/kanboard.user.js
+// @updateURL   https://github.com/gigaSproule/user-scripts/raw/master/kanboard.meta.js
 // @version     1
 // @grant       none
 // ==/UserScript==
