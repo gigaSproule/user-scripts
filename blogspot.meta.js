5a0b0bea96b5224d1bc0922b0eb6088ddf0113fc
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-14 19:26:03 +0000

    Created blogspot.meta.js

---
+++ blogspot.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Blogspot
+// @namespace   http://www.benjaminsproule.com
+// @author      Benjamin Sproule
+// @include     http://*.blogspot.co.uk/*
+// @match     http://*.blogspot.co.uk/*
+// @downloadURL https://github.com/gigaSproule/user-scripts/raw/master/blogspot.user.js
+// @updateURL   https://github.com/gigaSproule/user-scripts/raw/master/blogspot.meta.js
+// @version     1.0.0
+// @run-at      document-end
+// ==/UserScript==
