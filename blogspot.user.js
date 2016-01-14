26b9b8597c697b1d206789f37e1acd8ac6ca3fdb
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-14 19:26:23 +0000

    Updated metadata block

--- blogspot.user.js
+++ blogspot.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name        Blogspot
-// @namespace   com.benjaminsproule
+// @namespace   http://www.benjaminsproule.com
+// @author      Benjamin Sproule
 // @include     http://*.blogspot.co.uk/*
+// @match       http://*.blogspot.co.uk/*
 // @downloadURL https://github.com/gigaSproule/user-scripts/raw/master/blogspot.user.js
-// @version     1
-// @grant       none
+// @updateURL   https://github.com/gigaSproule/user-scripts/raw/master/blogspot.meta.js
+// @version     1.0.0
+// @run-at      document-end
 // ==/UserScript==
 document.onkeydown = function(event) {
   if(event.keyCode == '37') {
