3a2f55251d0cde12c5164e734cdc971d73234d5e
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-14 17:10:32 +0000

    Created kanboard.meta.js

---
+++ kanboard.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        Kanboard
+// @namespace   com.benjaminsproule
+// @include     http://*/kanboard*
+// @downloadURL https://github.com/gigaSproule/user-scripts/raw/master/kanboard.user.js
+// @updateURL   https://github.com/gigaSproule/user-scripts/raw/master/kanboard.meta.js
+// @version     1
+// @grant       none
+// ==/UserScript==
