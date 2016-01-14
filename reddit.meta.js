77765dfe1d2ebe494d13f10c7d0351bde73b697e
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-14 23:08:33 +0000

    Updating version

--- reddit.meta.js
+++ reddit.meta.js
@@ -2,7 +2,7 @@
 // @name        Reddit
 // @namespace   http://www.benjaminsproule.com
 // @author      Benjamin Sproule
-// @version     1.0.1
+// @version     1.0.3
 // @include     http://*.reddit.com/*
 // @include     https://*.reddit.com/*
 // @match       http://*.reddit.com/*
