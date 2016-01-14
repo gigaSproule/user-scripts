d3f0c5b6a408987635b45f744ad35571ac2546eb
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-14 22:45:53 +0000

    Testing update

--- reddit.user.js
+++ reddit.user.js
@@ -2,7 +2,7 @@
 // @name        Reddit
 // @namespace   http://www.benjaminsproule.com
 // @author      Benjamin Sproule
-// @version     1.0.1
+// @version     1.0.2
 // @include     http://*.reddit.com/*
 // @include     https://*.reddit.com/*
 // @match       http://*.reddit.com/*
@@ -14,6 +14,7 @@
 // @require     https://github.com/gigaSproule/user-scripts/raw/master/utils.js
 // ==/UserScript==
 window.onload = function () {
+    console.log('clickByQuerySelector ' + clickByQuerySelector);
     bindKey('37', clickByQuerySelector('a[rel="nofollow prev"]'));
     bindKey('39', clickByQuerySelector('a[rel="nofollow next"]'));
 };
