077a56bd4ed9e4ef3ca5229d061057935f1aa93d
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-01-31 23:53:18 +0000

    Trying to get the scripts to work with Tampermonkey

--- utils.js
+++ utils.js
@@ -1,105 +1,103 @@
 /* jshint esversion:6 */
 /* jshint strict:true */
-var utils = {
-    keyBinds: {},
-    altKeyBinds: {},
-    metaKeyBinds: {},
-    shiftKeyBinds: {},
-    altMetaKeyBinds: {},
-    shiftMetaKeyBinds: {},
-    shouldCatchKeyCodes: false,
-    caughtKeyCodes: [],
-    keyMap: {
-        '48': 0,
-        '49': 1,
-        '50': 2,
-        '51': 3,
-        '52': 4,
-        '53': 5,
-        '54': 6,
-        '55': 7,
-        '56': 8,
-        '57': 9,
-        '65': 'a',
-        '66': 'b',
-        '67': 'c',
-        '68': 'd',
-        '69': 'e',
-        '70': 'f',
-        '71': 'g',
-        '72': 'h',
-        '73': 'i',
-        '74': 'j',
-        '75': 'k',
-        '76': 'l',
-        '77': 'm',
-        '78': 'n',
-        '79': 'o',
-        '80': 'p',
-        '81': 'q',
-        '82': 'r',
-        '83': 's',
-        '84': 't',
-        '85': 'u',
-        '86': 'v',
-        '87': 'w',
-        '88': 'x',
-        '89': 'y',
-        '90': 'z'
-    }
+var keyBinds = {};
+var altKeyBinds = {};
+var metaKeyBinds = {};
+var shiftKeyBinds = {};
+var altMetaKeyBinds = {};
+var shiftMetaKeyBinds = {};
+var shouldCatchKeyCodes = false;
+var caughtKeyCodes = [];
+var keyMap = {
+    '48': 0,
+    '49': 1,
+    '50': 2,
+    '51': 3,
+    '52': 4,
+    '53': 5,
+    '54': 6,
+    '55': 7,
+    '56': 8,
+    '57': 9,
+    '65': 'a',
+    '66': 'b',
+    '67': 'c',
+    '68': 'd',
+    '69': 'e',
+    '70': 'f',
+    '71': 'g',
+    '72': 'h',
+    '73': 'i',
+    '74': 'j',
+    '75': 'k',
+    '76': 'l',
+    '77': 'm',
+    '78': 'n',
+    '79': 'o',
+    '80': 'p',
+    '81': 'q',
+    '82': 'r',
+    '83': 's',
+    '84': 't',
+    '85': 'u',
+    '86': 'v',
+    '87': 'w',
+    '88': 'x',
+    '89': 'y',
+    '90': 'z'
 };
 
 document.addEventListener('keydown', function (event) {
     'use strict';
     var metaKey = navigator.platform.toLowerCase().contains('mac') ? event.metaKey : event.ctrlKey;
-    if (utils.shouldCatchKeyCodes) {
-        utils.caughtKeyCodes.push(event.keyCode);
+    if (shouldCatchKeyCodes) {
+        caughtKeyCodes.push(event.keyCode);
         return;
     }
     if (!metaKey && !event.altKey && !event.shiftKey) {
-        if (utils.keyBinds[event.keyCode] === undefined) {
+        if (keyBinds[event.keyCode] === undefined) {
             return;
         }
-        for (let key of utils.keyBinds[event.keyCode]) {
+        for (let key of keyBinds[event.keyCode]) {
             key();
         }
     } else if (!metaKey && event.altKey && !event.shiftKey) {
-        if (utils.altKeyBinds[event.keyCode] === undefined) {
+        if (altKeyBinds[event.keyCode] === undefined) {
             return;
         }
-        for (let key of utils.altKeyBinds[event.keyCode]) {
+        for (let key of altKeyBinds[event.keyCode]) {
             event.preventDefault();
             key();
         }
     } else if (metaKey && !event.altKey && !event.shiftKey) {
-        if (utils.metaKeyBinds[event.keyCode] === undefined) {
+        if (metaKeyBinds[event.keyCode] === undefined) {
             return;
         }
-        for (let key of utils.metaKeyBinds[event.keyCode]) {
+        for (let key of metaKeyBinds[event.keyCode]) {
             event.preventDefault();
             key();
         }
     } else if (!metaKey && !event.altKey && event.shiftKey) {
-        if (utils.shiftKeyBinds[event.keyCode] === undefined) {
+        if (shiftKeyBinds[event.keyCode] === undefined) {
             return;
         }
-        for (let key of utils.shiftKeyBinds[event.keyCode]) {
+        for (let key of shiftKeyBinds[event.keyCode]) {
             event.preventDefault();
             key();
         }
     } else if (metaKey && event.shiftKey && !event.altKey) {
-        if (utils.shiftMetaKeyBinds[event.keyCode] === undefined) {
+        if (shiftMetaKeyBinds[event.keyCode] === undefined) {
             return;
         }
-        for (let key of utils.shiftMetaKeyBinds[event.keyCode]) {
+        for (let key of shiftMetaKeyBinds[event.keyCode]) {
             event.preventDefault();
             key();
         }
     } else if (metaKey && !event.shiftKey && event.altKey) {
-        if (utils.altMetaKeyBinds[event.keyCode] === undefined) {
+        if (altMetaKeyBinds[event.keyCode] === undefined) {
             return;
         }
-        for (let key of utils.altMetaKeyBinds[event.keyCode]) {
+        for (let key of altMetaKeyBinds[event.keyCode]) {
             event.preventDefault();
             key();
         }
@@ -108,60 +106,60 @@ document.addEventListener('keydown', function (event) {
 
 function bindKey(keyCode, func) {
     'use strict';
-    if (utils.keyBinds[keyCode] === null) {
-        utils.keyBinds[keyCode] = [];
+    if (keyBinds[keyCode] === null) {
+        keyBinds[keyCode] = [];
     }
-    utils.keyBinds[keyCode].push(func);
+    keyBinds[keyCode].push(func);
 }
 
 function bindAltKey(keyCode, func) {
     'use strict';
-    if (utils.altKeyBinds[keyCode] === null) {
-        utils.altKeyBinds[keyCode] = [];
+    if (altKeyBinds[keyCode] === null) {
+        altKeyBinds[keyCode] = [];
     }
-    utils.altKeyBinds[keyCode].push(func);
+    altKeyBinds[keyCode].push(func);
 }
 
 function bindMetaKey(keyCode, func) {
     'use strict';
-    if (utils.metaKeyBinds[keyCode] === null) {
-        utils.metaKeyBinds[keyCode] = [];
+    if (metaKeyBinds[keyCode] === null) {
+        metaKeyBinds[keyCode] = [];
     }
-    utils.metaKeyBinds[keyCode].push(func);
+    metaKeyBinds[keyCode].push(func);
 }
 
 function bindShiftKey(keyCode, func) {
     'use strict';
-    if (utils.shiftKeyBinds[keyCode] === null) {
-        utils.shiftKeyBinds[keyCode] = [];
+    if (shiftKeyBinds[keyCode] === null) {
+        shiftKeyBinds[keyCode] = [];
     }
-    utils.shiftKeyBinds[keyCode].push(func);
+    shiftKeyBinds[keyCode].push(func);
 }
 
 function bindAltMetaKey(keyCode, func) {
     'use strict';
-    if (utils.altMetaKeyBinds[keyCode] === null) {
-        utils.altMetaKeyBinds[keyCode] = [];
+    if (altMetaKeyBinds[keyCode] === null) {
+        altMetaKeyBinds[keyCode] = [];
     }
-    utils.altMetaKeyBinds[keyCode].push(func);
+    altMetaKeyBinds[keyCode].push(func);
 }
 
 function bindShiftMetaKey(keyCode, func) {
     'use strict';
-    if (utils.shiftMetaKeyBinds[keyCode] === null) {
-        utils.shiftMetaKeyBinds[keyCode] = [];
+    if (shiftMetaKeyBinds[keyCode] === null) {
+        shiftMetaKeyBinds[keyCode] = [];
     }
-    utils.shiftMetaKeyBinds[keyCode].push(func);
+    shiftMetaKeyBinds[keyCode].push(func);
 }
 
 function catchKeyCodes(func, timeout) {
     'use strict';
-    utils.shouldCatchKeyCodes = true;
+    shouldCatchKeyCodes = true;
     setTimeout(function () {
-        utils.caughtKeyCodes.shift();
-        func(utils.caughtKeyCodes);
-        utils.caughtKeyCodes = [];
-        utils.shouldCatchKeyCodes = false;
+        caughtKeyCodes.shift();
+        func(caughtKeyCodes);
+        caughtKeyCodes = [];
+        shouldCatchKeyCodes = false;
     }, timeout);
 }
 
@@ -197,7 +195,7 @@ function clickByQuerySelector(querySelector, text) {
 
 function mapKey(key) {
     'use strict';
-    return utils.keyMap[key];
+    return keyMap[key];
 }
 
 function updateSelectById(id, value) {
