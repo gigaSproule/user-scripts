ce1d67942109135eb0d15c3c5b29656c2f3ef840
Benjamin Sproule <benjamin@benjaminsproule.com>
2016-08-30 15:02:50 +0100

    Create github.meta.js

---
+++ github.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @version     1
+// @grant       none
+// ==/UserScript==
+// ==UserScript==
+// @name        Full width PR for GitHub
+// @namespace   http://www.benjaminsproule.com
+// @description Full width PR for GitHub
+// @author      Benjamin Sproule
+// @version     1.0.13
+// @include     https://github.com/*/pull/*
+// @match     https://github.com/*/pull/*
+// @downloadURL https://github.com/gigaSproule/user-scripts/raw/master/github.user.js
+// @updateURL   https://github.com/gigaSproule/user-scripts/raw/master/github.meta.js
+// @grant       none
+// @run-at      document-end
+// @require     https://github.com/gigaSproule/user-scripts/raw/master/utils.js
+// ==/UserScript==
+/* jshint esversion:6 */
+/* jshint strict:true */
