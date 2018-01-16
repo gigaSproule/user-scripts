5d56e9636d3bf061f880da94a378141e9dc99cc8
Benjamin Sproule <benjamin.sproule@veritas.com>
2018-01-16 15:14:06 +0000

    Added copy clone command

--- stash.user.js
+++ stash.user.js
@@ -15,34 +15,56 @@
 window.onload = function () {
     'use strict';
 
-    var cancelStandardCopy = false;
+    var copyCheckoutCommand = false;
+    var copyCloneCommand = false;
 
     document.addEventListener('copy', function (e) {
-        console.log('Cancel standard copy: ' + cancelStandardCopy);
-        if (cancelStandardCopy) {
+        if (copyCheckoutCommand) {
             e.clipboardData.setData('text/plain', 'git checkout ' + document.querySelector('div.ref-name:nth-child(1) > span:nth-child(1)').innerHTML);
             e.preventDefault();
-            cancelStandardCopy = false;
+            copyCheckoutCommand = false;
+        } else if (copyCloneCommand) {
+            e.clipboardData.setData('text/plain', 'git clone ' + document.querySelector('.quick-copy-text').value);
+            e.preventDefault();
+            copyCloneCommand = false;
         }
-        console.log('Cancel standard copy: ' + cancelStandardCopy);
     });
 
-    var button = document.createElement('button');
-    button.className = 'aui-button aui-button-link';
-    button.setAttribute('role', 'menuitem');
-    button.setAttribute('data-action', 'copy-checkout-command');
-    button.tabIndex = -1;
-    button.innerText = 'Copy checkout command';
-    button.addEventListener('click', function () {
+    var checkoutCommandButton = document.createElement('button');
+    checkoutCommandButton.className = 'aui-button aui-button-link';
+    checkoutCommandButton.setAttribute('role', 'menuitem');
+    checkoutCommandButton.setAttribute('data-action', 'copy-checkout-command');
+    checkoutCommandButton.tabIndex = -1;
+    checkoutCommandButton.innerText = 'Copy checkout command';
+    checkoutCommandButton.addEventListener('click', function () {
         document.execCommand('copy');
     });
 
-    var item = document.createElement('li');
-    item.appendChild(button);
-    document.querySelector('#pull-request-header-more > div:nth-child(1) > ul:nth-child(1)').appendChild(item);
+    var checkoutCommandItem = document.createElement('li');
+    checkoutCommandItem.appendChild(checkoutCommandButton);
+    document.querySelector('#pull-request-header-more > div:nth-child(1) > ul:nth-child(1)').appendChild(checkoutCommandButton);
+
+    var cloneCommandButton = document.createElement('button');
+    cloneCommandButton.className = 'aui-button aui-button-link';
+    cloneCommandButton.setAttribute('role', 'menuitem');
+    cloneCommandButton.setAttribute('data-action', 'copy-clone-command');
+    cloneCommandButton.tabIndex = -1;
+    cloneCommandButton.innerText = 'Copy clone command';
+    cloneCommandButton.addEventListener('click', function () {
+        document.execCommand('copy');
+    });
+
+    var cloneCommandItem = document.createElement('li');
+    cloneCommandItem.appendChild(cloneCommandButton);
+    document.querySelector('#pull-request-header-more > div:nth-child(1) > ul:nth-child(1)').appendChild(cloneCommandItem);
 
     bindAltKey('67', 'Copy checkout command', function () {
-        cancelStandardCopy = true;
+        copyCheckoutCommand = true;
+        document.execCommand('copy');
+    });
+
+    bindAltKey('78', 'Copy clone command', function () {
+        copyCloneCommand = true;
         document.execCommand('copy');
     });
 };
